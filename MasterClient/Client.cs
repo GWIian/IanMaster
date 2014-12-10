8d57ba185b94266e9fd493789e39c58a58a9f7c1
GWIian <chinahwz@qq.com>
2014-12-10 15:34:49 +0800

    添加了客户端配置文件

---
+++ MasterClient/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MasterClient
+{
+    class Client
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
